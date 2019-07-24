f09177933f395ea5cbf96ce8aace402a4bb1b28c
chetan4947 <52272833+chetan4947@users.noreply.github.com>
2019-07-24 12:29:20 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Palindrome {
+	public static void main(String args[])
+	{
+		int num;
+		int sum=0;
+		int num1;
+		int rem;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Your Number:");
+		num = sc.nextInt();
+		num1 = num;
+		while(num1 > 0)
+		{
+		rem = num1 % 10;
+		sum = (sum * 10) + rem;
+		num1 = num1 / 10;
+		}
+		if(num == sum)
+			{
+				System.out.println("Your given number is Palindrome Number.");
+			}
+		else
+			{
+				System.out.println("Your given number is not Palindrome Number");
+			}
+	}
+
+}
